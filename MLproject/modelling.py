77a42bc5d01c2a3c3e35d761fe8aea644118ee14
kikio110 <sahanaturrizki@gmail.com>
2025-06-26 10:20:40 +0700

    feat:tes5

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -1,41 +1,51 @@
-# Import library
 import os
+import argparse
+import pandas as pd
 import mlflow
 import mlflow.sklearn
-import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-# Ambil nilai dari environment variable (jika ada)
+# Argument parser untuk menerima parameter dari MLproject atau CLI
+parser = argparse.ArgumentParser()
+parser.add_argument("n_estimators", type=int)
+parser.add_argument("max_depth", type=int)
+parser.add_argument("dataset", type=str)
+args = parser.parse_args()
+
+# Set tracking URI hanya jika disediakan melalui environment
 tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
 if tracking_uri:
     mlflow.set_tracking_uri(tracking_uri)
 
-# Set nama eksperimen
+# Set eksperimen
 mlflow.set_experiment("Eksperimen klasifikasi berat badan")
 
 # Baca data
-data = pd.read_csv("Obesity Classification Preprocessing.csv")
+data = pd.read_csv(args.dataset)
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(
     data.drop("Label", axis=1),
     data["Label"],
-    random_state=42,
-    test_size=0.2
+    test_size=0.2,
+    random_state=42
 )
 
-# Input contoh untuk autologging (opsional)
-input_example = X_train[0:5]
-
-# Aktifkan autologging
+# Aktifkan autolog
 mlflow.sklearn.autolog()
 
-# Mulai run
-with mlflow.start_run():
-    model = RandomForestClassifier(n_estimators=505, max_depth=37)
+# Cek apakah sedang dijalankan oleh mlflow CLI (otomatis sudah dalam run)
+# Jika tidak, jalankan manual start_run
+if mlflow.active_run() is None:
+    with mlflow.start_run():
+        model = RandomForestClassifier(n_estimators=args.n_estimators, max_depth=args.max_depth)
+        model.fit(X_train, y_train)
+        accuracy = model.score(X_test, y_test)
+        print("Akurasi:", accuracy)
+else:
+    # Sudah dalam MLflow run, langsung jalan
+    model = RandomForestClassifier(n_estimators=args.n_estimators, max_depth=args.max_depth)
     model.fit(X_train, y_train)
-
-    # Evaluasi
     accuracy = model.score(X_test, y_test)
-    print("Accuracy:", accuracy)
+    print("Akurasi:", accuracy)
